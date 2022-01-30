d256fd5a9b3dc8cee8f0b60a3ff312827c0e346f
asadullah097 <asaddir5566@gmail.com>
2022-01-30 16:34:40 +0500

    type inference

---
+++ 04/index.ts
@@ -0,0 +1,13 @@
+let guest = ['Joe', 'Jeni', 'John', 22, true]
+console.log(typeof guest);
+
+
+// let myName = "Sarthak"
+// myName = 2022
+// console.log(typeof myName);
+
+// let year
+// year = 2022
+// year = "abcd"
+// console.log(typeof year);
+// type Inference
