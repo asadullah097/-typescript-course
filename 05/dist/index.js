c83be53a33c65483b211a48cac8722c957361fe6
asadullah097 <asaddir5566@gmail.com>
2022-01-30 19:59:39 +0500

    tsconfig init

---
+++ 05/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function PrintName(my_name) {
+    return my_name;
+}
+console.log(PrintName("asad"));
---
+++ 05/index.ts
@@ -0,0 +1,5 @@
+
+function PrintName(my_name:string):string{
+    return my_name  
+}
+console.log(PrintName("asad"));
