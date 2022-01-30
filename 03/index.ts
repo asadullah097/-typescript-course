359e5b766f585f0ee670f58d23f0f4306ab8f7f7
asadullah097 <asaddir5566@gmail.com>
2022-01-30 16:02:41 +0500

    Define Types

---
+++ 03/index.ts
@@ -0,0 +1,25 @@
+// PRIMITIVE TYPES
+// string
+let myName: string = "Asad"
+// number
+let year: number = 2022
+// boolean
+let isEnable: boolean
+isEnable = true
+// null
+// undefined
+// symbol
+
+// OBJECT TYPES
+// arrays, objects, clases
+
+// Types for Array
+let guests: string[] = ['Joe', 'Jeni', 'John']
+
+// Types for a function
+// let killBill: (name: string) => string;
+
+const killBill = function (name: string): string {
+    return name.toUpperCase()
+}
+console.log(killBill("asad"));
